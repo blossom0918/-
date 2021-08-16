12f57e01eb7039302ffc71ece2ff5004d5b658ca
blossom0918 <39674227+blossom0918@users.noreply.github.com>
2021-08-16 22:37:05 +0800

    Add files via upload

---
+++ 翻譯蒟蒻.py
@@ -0,0 +1,29 @@
+﻿import speech_recognition
+import tempfile
+from gtts import gTTS
+import mp3play
+import time
+from googletrans import Translator
+
+def listenTo():
+    r = speech_recognition.Recognizer()
+
+    with speech_recognition.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+
+    return r.recognize_google(audio, language='zh-TW')
+
+
+def speak(sentence, lang):
+    with tempfile.NamedTemporaryFile(delete=True) as fp:
+		tts = gTTS(text=sentence, lang=lang)
+		tts.save("{}.mp3".format(fp.name))
+		clip = mp3play.load('{}.mp3'.format(fp.name))
+		clip.play()
+		time.sleep(3)
+		clip.stop()
+		
+translator = Translator()
+lang = 'en'
+speak(translator.translate(listenTo(), lang).text, lang)
